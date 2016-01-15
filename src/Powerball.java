37341544522c194ca9aa1a21c4139c2988169060
Guyon Cumby <thetubbycat@gmail.com>
2016-01-15 16:27:57 -0500

    Consolidation
    Consolidated some repeated variables into arrays, added winning number generation, and commented

--- src/Powerball.java
+++ src/Powerball.java
@@ -7,20 +7,25 @@ public class Powerball {
 		// User variables
 		// If you're running high numbers, you may want to comment out some System.out
 		// statements of higher probability outcomes
-		int   tries   = 50;						// $2 per try, mind you
+		int   tries   = 50;			 		// $2 per try, mind you
 		int   jackpot = 1500000000;			    // Amount won in the jackpot
 		int[] winning = {4, 8, 19, 27, 34, 10}; // Winning numbers, last being Powerball
-		
+		boolean presetWin = false;				// if you don't want to use the winning numbers
+												// above, set to false and winning numbers will
+												// be generated
 		// Non-user variables
 		int randomNum;
 		int payout          = 0;
 		int[] choices       = new int[5]; 		// the non-Powerball #'s
 		int[] winningCombos = new int[9]; 		// 9 different ways to win
+		int[] comboAmounts  = {jackpot, 1000000, 50000, 100, 100, 7, 7, 4, 4};				   // Amount won per combination
+		String[] comboNames	= {"5+1", "5+0", "4+1", "4+0", "3+1", "3+0", "2+1", "1+1", "0+1"}; // Different combinations
+		
 		for (int i = 0; i < tries; i++) {
-			Arrays.fill(choices, 0);			// reset
+			Arrays.fill(choices, 0);			// reset the array
 			Random rand = new Random();
 			
-			// Generate five unique pseudo-random numbers 1-69
+			// Generate five unique pseudo-random numbers 1-69 (Inclusive)
 			choices[0] = rand.nextInt(69) + 1;
 			while (choices[1] == 0) {
 				randomNum = rand.nextInt(69) + 1;
@@ -30,13 +35,13 @@ public class Powerball {
 			}
 			while (choices[2] == 0) {
 				randomNum = rand.nextInt(69) + 1;
-				if (randomNum != choices[0] && randomNum != choices[1] ) {
+				if (randomNum != choices[0] && randomNum != choices[1]) {
 					choices[2] = randomNum;
 				}
 			}
 			while (choices[3] == 0) {
 				randomNum = rand.nextInt(69) + 1;
-				if (randomNum != choices[0] && randomNum != choices[1] && randomNum != choices[2] ) {
+				if (randomNum != choices[0] && randomNum != choices[1] && randomNum != choices[2]) {
 					choices[3] = randomNum;
 				}
 			}
@@ -47,13 +52,31 @@ public class Powerball {
 				}
 			}
 			
-			// Generate the Powerball (1-26)
+			// Generate the Powerball (1-26, Inclusive)
 			int powerball = rand.nextInt(26) + 1;
 			
 			// Sort 'em
 			Arrays.sort(choices, 0, 5);
 			
-			// See if you're a winner
+			// If the user didn't want the winning numbers to be preset,
+			// use the first generation as the winning number choice
+			if (!presetWin) {
+				// Steal each ball
+				for (int j = 0; j < 5; j++) {
+					winning[j] = choices[j];
+				}
+				// Steal the Powerball
+				winning[5] = powerball;
+				// Make sure we don't do this again
+				presetWin = true;
+				// Give this turn back
+				i--;
+				// Bail
+				continue;
+			}
+			
+			// Check if any of the winning numbers (sans Powerball)
+			// were in our set
 			int matches = 0;
 			if (Arrays.binarySearch(choices, winning[0]) >= 0) {
 				matches++;
@@ -73,80 +96,65 @@ public class Powerball {
 			
 			// Print out if you won any money
 			if (matches == 5 && powerball == winning[5]) {
-				System.out.format("%2d %2d %2d %2d %2d %2d | JACKPOT on attempt %d%n", choices[0], choices[1], choices[2], choices[3], choices[4], powerball, i);
-				payout += jackpot;
+				System.out.format("%2d %2d %2d %2d %2d %2d | %s on attempt %d%n", choices[0], choices[1], choices[2], choices[3], choices[4], powerball, comboNames[0], i);
+				payout += comboAmounts[0];
 				winningCombos[0]++;
 			} else if (matches == 5) {
-				System.out.format("%2d %2d %2d %2d %2d %2d | 5+0 on attempt %d%n", choices[0], choices[1], choices[2], choices[3], choices[4], powerball, i);
-				payout += 1000000;
+				System.out.format("%2d %2d %2d %2d %2d %2d | %s on attempt %d%n", choices[0], choices[1], choices[2], choices[3], choices[4], powerball, comboNames[1], i);
+				payout += comboAmounts[1];
 				winningCombos[1]++;
 			} else if (matches == 4 && powerball == winning[5]) {
-				System.out.format("%2d %2d %2d %2d %2d %2d | 4+1 on attempt %d%n", choices[0], choices[1], choices[2], choices[3], choices[4], powerball, i);
-				payout += 50000;
+				System.out.format("%2d %2d %2d %2d %2d %2d | %s on attempt %d%n", choices[0], choices[1], choices[2], choices[3], choices[4], powerball, comboNames[2], i);
+				payout += comboAmounts[2];
 				winningCombos[2]++;
 			} else if (matches == 4) {
-				System.out.format("%2d %2d %2d %2d %2d %2d | 4+0 on attempt %d%n", choices[0], choices[1], choices[2], choices[3], choices[4], powerball, i);
-				payout += 100;
+				System.out.format("%2d %2d %2d %2d %2d %2d | %s on attempt %d%n", choices[0], choices[1], choices[2], choices[3], choices[4], powerball, comboNames[3], i);
+				payout += comboAmounts[3];
 				winningCombos[3]++;
 			} else if (matches == 3 && powerball == winning[5]) {
-				System.out.format("%2d %2d %2d %2d %2d %2d | 3+1 on attempt %d%n", choices[0], choices[1], choices[2], choices[3], choices[4], powerball, i);
-				payout += 100;
+				System.out.format("%2d %2d %2d %2d %2d %2d | %s on attempt %d%n", choices[0], choices[1], choices[2], choices[3], choices[4], powerball, comboNames[4], i);
+				payout += comboAmounts[4];
 				winningCombos[4]++;
 			} else if (matches == 3) {
-				System.out.format("%2d %2d %2d %2d %2d %2d | 3+0 on attempt %d%n", choices[0], choices[1], choices[2], choices[3], choices[4], powerball, i);
-				payout += 7;
+				System.out.format("%2d %2d %2d %2d %2d %2d | %s on attempt %d%n", choices[0], choices[1], choices[2], choices[3], choices[4], powerball, comboNames[5], i);
+				payout += comboAmounts[5];
 				winningCombos[5]++;
 			} else if (matches == 2 && powerball == winning[5]) {
-				System.out.format("%2d %2d %2d %2d %2d %2d | 2+1 on attempt %d%n", choices[0], choices[1], choices[2], choices[3], choices[4], powerball, i);
-				payout += 7;
+				System.out.format("%2d %2d %2d %2d %2d %2d | %s on attempt %d%n", choices[0], choices[1], choices[2], choices[3], choices[4], powerball, comboNames[6], i);
+				payout += comboAmounts[6];
 				winningCombos[6]++;
 			} else if (matches == 1 && powerball == winning[5]) {
-				System.out.format("%2d %2d %2d %2d %2d %2d | 1+1 on attempt %d%n", choices[0], choices[1], choices[2], choices[3], choices[4], powerball, i);
-				payout += 4;
+				System.out.format("%2d %2d %2d %2d %2d %2d | %s on attempt %d%n", choices[0], choices[1], choices[2], choices[3], choices[4], powerball, comboNames[7], i);
+				payout += comboAmounts[7];
 				winningCombos[7]++;
 			} else if (powerball == winning[5]) {
-				System.out.format("%2d %2d %2d %2d %2d %2d | 0+1 on attempt %d%n", choices[0], choices[1], choices[2], choices[3], choices[4], powerball, i);
-				payout += 4;
+				System.out.format("%2d %2d %2d %2d %2d %2d | %s on attempt %d%n", choices[0], choices[1], choices[2], choices[3], choices[4], powerball, comboNames[8], i);
+				payout += comboAmounts[8];
 				winningCombos[8]++;
 			}
 		}
+		
+		System.out.println("\nWinning numbers: " + winning[0] + " " + winning[1] + " " + winning[2] + 
+											 " " + winning[3] + " " + winning[4] + " " + winning[5]);
 		
 		// Print out final statistics by type of win
 		int totalWins =  winningCombos[8] + winningCombos[7]+ winningCombos[6] + winningCombos[5] + winningCombos[4] +
 			     winningCombos[3] + winningCombos[2] + winningCombos[1] + winningCombos[0];
 		if (totalWins > 0) {
 			System.out.println("\nTitle    |          # |          % | Winnings");
-			if (winningCombos[8] > 0) {
-				System.out.format ("0+1      | %10d | %1.8f | $%d%n", winningCombos[8], (float) winningCombos[8]/tries, winningCombos[8]*4);
+			
+			for (int i = 0; i < 9; i++) {
+				if (winningCombos[i] > 0) {
+					System.out.format ("%s      | %10d | %1.8f | $%d%n", comboNames[i], winningCombos[i],
+									   (float) winningCombos[i]/tries, winningCombos[i]*comboAmounts[i]);
+				}
 			}
-			if (winningCombos[7] > 0) {
-				System.out.format ("1+1      | %10d | %1.8f | $%d%n", winningCombos[7], (float) winningCombos[7]/tries, winningCombos[7]*4);
-			}
-			if (winningCombos[6] > 0) {
-				System.out.format ("2+1      | %10d | %1.8f | $%d%n", winningCombos[6], (float) winningCombos[6]/tries, winningCombos[6]*7);
-			}
-			if (winningCombos[5] > 0) {
-				System.out.format ("3        | %10d | %1.8f | $%d%n", winningCombos[5], (float) winningCombos[5]/tries, winningCombos[5]*7);
-			}
-			if (winningCombos[4] > 0) {
-				System.out.format ("3+1      | %10d | %1.8f | $%d%n", winningCombos[4], (float) winningCombos[4]/tries, winningCombos[4]*100);
-			}
-			if (winningCombos[3] > 0) {
-				System.out.format ("4        | %10d | %1.8f | $%d%n", winningCombos[3], (float) winningCombos[3]/tries, winningCombos[3]*100);
-			}
-			if (winningCombos[2] > 0) {
-				System.out.format ("4+1      | %10d | %1.8f | $%d%n", winningCombos[2], (float) winningCombos[2]/tries, winningCombos[2]*50000);
-			}
-			if (winningCombos[1] > 0) {
-				System.out.format ("5        | %10d | %1.8f | $%d%n", winningCombos[1], (float) winningCombos[1]/tries, winningCombos[1]*1000000);
-			}
-			if (winningCombos[0] > 0) {
-				System.out.format ("5+1      | %10d | %1.8f | $%d%n", winningCombos[0], (float) winningCombos[0]/tries, winningCombos[0]*jackpot);
-			}
+
 			float totalPercent = (float) (winningCombos[8] + winningCombos[7]+ winningCombos[6] + winningCombos[5] + winningCombos[4] +
 							     winningCombos[3] + winningCombos[2] + winningCombos[1] + winningCombos[0])/tries;
-			int totalWinnings = winningCombos[8]*4 + winningCombos[7]*4 +  winningCombos[6]*7 +  winningCombos[5]*7 + winningCombos[4]*100 + 
-								winningCombos[3]*100 + winningCombos[2]*50000 + winningCombos[1]*1000000 + winningCombos[0]*jackpot;
+			int totalWinnings = winningCombos[8]*comboAmounts[8] + winningCombos[7]*comboAmounts[7] + winningCombos[6]*comboAmounts[6] + 
+								winningCombos[5]*comboAmounts[5] + winningCombos[4]*comboAmounts[4] + winningCombos[3]*comboAmounts[3] +
+								winningCombos[2]*comboAmounts[2] + winningCombos[1]*comboAmounts[1] + winningCombos[0]*comboAmounts[0];
 			System.out.format("Total    | %10d | %1.8f | $%d%n%n", totalWins, totalPercent, totalWinnings);
 		}
 		
